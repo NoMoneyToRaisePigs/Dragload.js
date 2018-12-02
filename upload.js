09e3699b4a1ef30b6aa2e48767d23939b1a9052b
gaofan <gaofan19901103@gmail.com>
2018-12-02 23:40:50 +0800

    drapload first upload

---
+++ upload.js
@@ -0,0 +1,62 @@
+var area = document.getElementById('area');
+var input = document.getElementById('input');
+
+input.onchange = function(event){
+    var input = this;
+ 
+    // if (input.files && input.files[0]) {
+    //     var reader = new FileReader();
+    
+    //     reader.onload = function(e) {
+        
+    //     }
+ 
+    //     reader.readAsDataURL(input.files[0]);
+    // }
+
+    previewFile(input.files);
+};
+
+area.addEventListener('dragenter', function(event){
+    console.log('dragenter');
+    event.preventDefault();
+});
+
+area.addEventListener('dragleave', function(event){
+    console.log('dragleave');
+    event.preventDefault();
+});
+
+area.addEventListener('dragover', function(event){
+    console.log('dragover');
+    event.preventDefault();
+});
+
+area.addEventListener('drop', function(event){
+    console.log('drop');
+
+    let dt = event.dataTransfer;
+    let files = dt.files;
+  
+    previewFile(files);
+
+    event.preventDefault();
+});
+
+function previewFile(files){
+    if(input.files && input.files[0]){
+        ([...files]).forEach(function(file){
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = function() {
+              let img = document.createElement('img');
+              img.src = reader.result;
+              area.appendChild(img);
+            }
+        });
+    } 
+}
+
+function handleFiles(files) {
+    
+}
